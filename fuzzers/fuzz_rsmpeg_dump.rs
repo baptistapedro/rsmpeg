00cb21ab5a406a29e48ceb3beea5cc90a0283ffa
baptistapedro <pedrojorgesbaptista@gmail.com>
2022-12-13 00:42:07 +0100

    Create fuzz_rsmpeg_dump.rs

---
+++ fuzzers/fuzz_rsmpeg_dump.rs
@@ -0,0 +1,19 @@
+//#[macro_use]
+//extern crate afl;
+extern crate rsmpeg;
+
+use std::ffi::{CStr, CString};
+use std::error::Error;
+use rsmpeg::avformat::AVFormatContextInput;
+use std::env;
+
+fn dump_av_info(path: &CStr) -> Result<(), Box<dyn Error>> {
+    let mut input_format_context = AVFormatContextInput::open(path)?;
+    input_format_context.dump(0, path)?;
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    dump_av_info(&CString::new(&*args[1]).unwrap()).unwrap();
+}
